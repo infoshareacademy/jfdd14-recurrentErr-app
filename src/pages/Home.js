42c1609f462e6bdbb5fc0995d3be7829a68a4a86
Piotr Kielak <p.kielak@outlook.com>
2020-03-13 16:56:15 +0100

    add cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import App from '../App';
+import { Card } from 'semantic-ui-react';
+
+const items = [
+    {
+      header: 'Rodzaje wędrówek',
+      description:
+        'Leverage agile frameworks to provide a robust synopsis for high level overviews.',
+      meta: 'Procentowy udział wędrówek w zależności od spędzonego czasu',
+    },
+    {
+      header: 'Czas wędrówek',
+      description:
+        'Bring to the table win-win survival strategies to ensure proactive domination.',
+      meta: 'Ile czasu poświęciłeś w tygodniu na wędrówki',
+    },
+];
 
 function Home(){
     return( 
     <div>
         <App />
+        <Card.Group centered fluid items={items} />
     </div>)
 }
 
