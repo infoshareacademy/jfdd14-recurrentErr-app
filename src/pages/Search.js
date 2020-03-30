c78a73092e5d93e2080888aa3deb4737497e4508
Piotr Kielak <p.kielak@outlook.com>
2020-03-31 00:56:10 +0200

    add form inputs

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Header, Grid, Form } from "semantic-ui-react";
+import SearchSlider from "../Components/SearchForm/SearchSlider";
+
+const diffOpt = [
+  { key: "ez", value: "easy", text: "Łatwa" },
+  { key: "it", value: "intermediate", text: "Średnia" },
+  { key: "hr", value: "hard", text: "Trudna" },
+];
 
 function Search() {
-  return <div></div>;
+  return (
+    <React.Fragment>
+      <Grid centered padded>
+        <Grid.Column as={Form} width={8} verticalAlign="middle">
+          <Header as="h2">Wyszukaj trasę:</Header>
+          <Form.Input action="Search" placeholder="Search..." />
+          <Form.Select
+            placeholder="Wybierz poziom trudności trasy"
+            options={diffOpt}
+          />
+          <SearchSlider />
+        </Grid.Column>
+      </Grid>
+    </React.Fragment>
+  );
 }
 
 export default Search;
