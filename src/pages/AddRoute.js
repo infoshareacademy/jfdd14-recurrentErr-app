9e47f0802c5143f31b535429251a9ca168b16aaa
paula-dm <paulina.ziomkowska@gmail.com>
2020-04-03 16:24:02 +0200

    form saves the input, some new fields required

--- src/pages/AddRoute.js
+++ src/pages/AddRoute.js
@@ -4,16 +4,15 @@ import {
   Checkbox,
   Form,
   Input,
-  Radio,
   Select,
   TextArea,
   Header,
 } from "semantic-ui-react";
 
 const options = [
-  { key: "easy", text: "Łatwy", value: "Łatwy" },
-  { key: "middle", text: "Średni", value: "Średni" },
-  { key: "difficult", text: "Trudny", value: "Trudny" },
+  { key: "1", text: "Łatwy", value: "Łatwy" },
+  { key: "2", text: "Średni", value: "Średni" },
+  { key: "3", text: "Trudny", value: "Trudny" },
 ];
 
 class AddRoute extends Component {
@@ -21,12 +20,12 @@ class AddRoute extends Component {
     averageSpeedV: "",
     cityV: "",
     distanceV: "",
-    levelV: "",
     nameV: "",
     photoV: "",
     timeV: "",
     descriptionV: "",
     routes: [],
+    levelV: "",
   };
 
   handleChangeDistance = (e) => {
@@ -47,11 +46,17 @@ class AddRoute extends Component {
   handleChangeTime = (e) => {
     this.setState({ timeV: e.target.value });
   };
+  handleChangeCity = (e) => {
+    this.setState({ cityV: e.target.value });
+  };
+  handleChangeLevel = (e) => {
+    this.setState({ levelV: e.target.value });
+  };
   handleChangeDescription = (e) => {
     this.setState({ descriptionV: e.target.value });
   };
 
-  handleChange = (e, { value }) => this.setState({ value });
+  handleChange = (e, { value }) => this.setState({ levelV: value });
 
   handleSubmit = (e) => {
     this.setState({
@@ -63,6 +68,8 @@ class AddRoute extends Component {
           time: this.state.timeV,
           speed: this.state.averageSpeedV,
           photo: this.state.photoV,
+          city: this.state.cityV,
+          level: this.state.levelV,
           description: this.state.descriptionV,
         },
       ],
@@ -71,6 +78,8 @@ class AddRoute extends Component {
       nameV: "",
       photoV: "",
       timeV: "",
+      cityV: "",
+      levelV: "",
       descriptionV: "",
     });
     e.preventDefault();
@@ -80,17 +89,17 @@ class AddRoute extends Component {
     this.state.routes.map((route) => (
       <div key={route.name}>
         <h2>Nazwa trasy: {route.name}</h2>
-        <h4>Dystans: {route.distance} km</h4>
-        <h4>Czas trwania: {route.time}min</h4>
-        <h4>Tempo: {route.speed}km/min</h4>
-        <h4>Zdjęcie: {route.photo}</h4>
-
+        <p>Miasto: {route.city}</p>
+        <p>Dystans: {route.distance} km</p>
+        <p>Poziom trudności: {route.level}</p>
+        <p>Zdjęcie: {route.photo}</p>
+        <p>Czas trwania: {route.time} min</p>
         <p>Krótki opis: {route.description}</p>
       </div>
     ));
 
   render() {
-    const { value } = this.state;
+    // const { value } = this.state;
     return (
       <div>
         <Form style={{ margin: 20 }} onSubmit={this.handleSubmit}>
@@ -111,43 +120,29 @@ class AddRoute extends Component {
             />
             <Form.Field
               control={Input}
+              label="Miasto"
+              placeholder="Wpisz nazwę miejsca"
+              value={this.state.cityV}
+              onChange={this.handleChangeCity}
+            />
+            <Form.Field
+              control={Input}
               label="Dystans (km)"
               placeholder="Podaj długość trasy"
               value={this.state.distanceV}
               onChange={this.handleChangeDistance}
             />
+          </Form.Group>
+
+          <Form.Group widths="3">
             <Form.Field
               control={Select}
               label="Poziom trudności"
               options={options}
               placeholder="Poziom trudności"
-            />
-          </Form.Group>
-          <Form.Group inline>
-            <label>Miasto</label>
-            <Form.Field
-              control={Radio}
-              label="Gdańsk"
-              value="1"
-              checked={value === "1"}
+              value={this.state.levelV}
               onChange={this.handleChange}
             />
-            <Form.Field
-              control={Radio}
-              label="Sopot"
-              value="2"
-              checked={value === "2"}
-              onChange={this.handleChange}
-            />
-            <Form.Field
-              control={Radio}
-              label="Gdynia"
-              value="3"
-              checked={value === "3"}
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <Form.Group widths="2">
             <Form.Field
               control={Input}
               label="Zdjęcie"
