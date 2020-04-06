75bfff2963303031e2aa1bab158e6a88de01c734
Piotr Kielak <p.kielak@outlook.com>
2020-04-06 15:22:57 +0200

    fix card buttons style

--- src/Components/SingleCard/SingleCard.js
+++ src/Components/SingleCard/SingleCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Icon, Grid } from "semantic-ui-react";
 
 function SingleCard(props) {
   return (
@@ -17,30 +18,41 @@ function SingleCard(props) {
             {props.item.city} || dystans: {props.item.distance}km
           </p>
           <p className="card_text">Stopień trudności: {props.item.level}</p>
-          <button
-            name={props.buttonName}
-            className="btn card_btn"
-            onClick={props.showModal}
-          >
-            Dowiedz się więcej...
-          </button>
-          {props.favPlaces.includes(props.item.key) ? (
-            <button
-              className="btn card_btn"
-              id={props.item.key}
-              onClick={props.onDelFavBtnClick}
-            >
-              Usuń z ulubionych
-            </button>
-          ) : (
-            <button
-              className="btn card_btn"
-              id={props.item.key}
-              onClick={props.onFavBtnClick}
-            >
-              Dodaj do ulubionych
-            </button>
-          )}
+          <Grid>
+            <Grid.Column width={8}>
+              {props.favPlaces.includes(props.item.key) ? (
+                <Button
+                  fluid
+                  icon
+                  id={props.item.key}
+                  onClick={props.onDelFavBtnClick}
+                  color="violet"
+                >
+                  <Icon name="heart" />
+                </Button>
+              ) : (
+                <Button
+                  fluid
+                  icon
+                  id={props.item.key}
+                  onClick={props.onFavBtnClick}
+                  color="green"
+                >
+                  <Icon name="heart outline" />
+                </Button>
+              )}
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <Button
+                fluid
+                icon
+                name={props.buttonName}
+                onClick={props.showModal}
+              >
+                <Icon name="info circle" />
+              </Button>
+            </Grid.Column>
+          </Grid>
         </div>
       </div>
     </li>
