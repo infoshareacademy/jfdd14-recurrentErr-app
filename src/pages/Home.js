4a2eef75c78a327b54a5197de33f01ab54931ce0
Karolina <karolina.szlenk@gmail.com>
2020-04-08 23:55:49 +0200

    Info about app simulation added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Header, Grid } from "semantic-ui-react";
+import { Header, Grid, Card } from "semantic-ui-react";
 import CustomBarChart from "../Components/Home/CustomBarChart.jsx";
 import CustomPieChart from "../Components/Home/CustomPieChart.jsx";
+import "../Components/Search/SearchForm.css";
+import "./Home.css";
 
 const items = {
   pie: {
@@ -32,7 +34,7 @@ const data = {
 function Home() {
   return (
     <React.Fragment>
-      <Grid centered stackable columns={2} padded>
+      <Grid centered stackable columns={2}>
         <Grid.Column width={16} verticalAlign="middle">
           <Header as="h2">
             Dzień dobry wędrowcze!
@@ -41,14 +43,52 @@ function Home() {
             </Header.Subheader>
           </Header>
         </Grid.Column>
-        <Grid.Column tablet={12} computer={7} largeScreen={6} stretched>
+        <Grid.Row>
+          <Grid.Column
+            as={Card}
+            className="cardElements"
+            textAlign="center"
+            tablet={12}
+            computer={14}
+            largeScreen={12}
+          >
+            <Header.Subheader>Info o apce</Header.Subheader>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel
+            diam in nulla hendrerit euismod sed vel magna. Nulla vel nibh felis.
+            Aliquam erat volutpat. Duis rutrum accumsan tristique. Ut sed ipsum
+            orci. Duis elit ligula, iaculis ac nunc ut, vestibulum vehicula
+            diam. In tristique interdum justo, in condimentum quam elementum a.
+            Cras vestibulum orci id lorem pellentesque pretium. Morbi malesuada
+            nunc et vulputate maximus. Quisque varius ligula a dapibus
+            tristique. Fusce dignissim velit at eros rhoncus gravida. Ut in
+            tortor convallis, sollicitudin ipsum sed, suscipit tortor. Phasellus
+            tempor, mauris ac auctor efficitur, dui leo euismod dolor, sit amet
+            congue dolor massa vitae felis. Vivamus sit amet leo aliquam,
+            egestas nisi nec, ornare risus. Ut vel commodo mi, in ultrices
+            tellus.
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Column
+          className="gridLeftColumn"
+          tablet={12}
+          computer={7}
+          largeScreen={6}
+          stretched
+        >
           <CustomPieChart
             pieHeader={items.pie.header}
             pieMeta={items.pie.meta}
             pieData={data.pie}
           />
         </Grid.Column>
-        <Grid.Column tablet={12} computer={7} largeScreen={6} stretched>
+        <Grid.Column
+          className="gridRightColumn"
+          tablet={12}
+          computer={7}
+          largeScreen={6}
+          stretched
+        >
           <CustomBarChart
             barHeader={items.bar.header}
             barMeta={items.bar.meta}
