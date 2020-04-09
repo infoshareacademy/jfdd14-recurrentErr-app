a27c210af561ede7af5d1cabe607908861e56b7f
Paweł Politowski <matelly@poczta.onet.pl>
2020-04-09 16:59:02 +0200

    Button in appbar changed

--- src/Components/Menu/AppbarReact.js
+++ src/Components/Menu/AppbarReact.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Menu, Responsive } from "semantic-ui-react";
+import { Menu, Responsive, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import SidebarReact from "./SidebarReact";
 import SidebarReactHamburger from "./SidebarReactHamburger";
@@ -30,18 +30,20 @@ class AppbarReact extends Component {
             onClick={this.changeMenuVisibility}
           ></Responsive>
           <Menu.Item as={Link} to="/" name="crossRoads" active>
-            <img src={logo} alt="logo" /> crossRoads
+            <img src={logo} alt="logo" />
+            crossRoads
           </Menu.Item>
 
           <Responsive
             as={Menu.Item}
             minWidth={768}
-            to="/"
             name="Zaloguj"
             active
             position="right"
           >
-            Logowanie
+            <Button inverted color="grey">
+              Logowanie
+            </Button>
           </Responsive>
           <Responsive
             as={Menu.Item}
