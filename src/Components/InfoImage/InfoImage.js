b7c8e965e3d969bd2f1f419d0cfa1182e53e65c9
Karolina <karolina.szlenk@gmail.com>
2020-04-10 06:16:47 +0200

    Image added

---
+++ src/Components/InfoImage/InfoImage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import infoImage from "./infoImage.svg";
+import "./InfoImage.css";
+
+function InfoImage() {
+  return <img className="imgStyle" src={infoImage} alt="peopleOnCrossroads" />;
+}
+
+export default InfoImage;
