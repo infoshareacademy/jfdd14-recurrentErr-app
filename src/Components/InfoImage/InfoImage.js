0a27affdc6c1d28d7f8917ba2cd16558c4009973
Karolina <karolina.szlenk@gmail.com>
2020-04-10 22:11:04 +0200

    Image style updated

--- src/Components/InfoImage/InfoImage.js
+++ src/Components/InfoImage/InfoImage.js
@@ -3,7 +3,7 @@ import infoImage from "./infoImage.svg";
 import "./InfoImage.css";
 
 function InfoImage() {
-  return <img className="imgStyle" src={infoImage} alt="peopleOnCrossroads" />;
+  return <img className="info__image" src={infoImage} alt="peopleOnCrossroads" />;
 }
 
 export default InfoImage;
