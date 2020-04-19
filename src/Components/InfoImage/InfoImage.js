ea894bd6447bf066cfa36eaa5a604dcf3b304a2f
Karolina <karolina.szlenk@gmail.com>
2020-04-19 19:36:08 +0200

    infoImage for mobile added

--- src/Components/InfoImage/InfoImage.js
+++ src/Components/InfoImage/InfoImage.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import infoImage from "./infoImage.svg";
+import infoImage_mobile from "./infoImage_mobile.svg";
 import "./InfoImage.css";
 
 function InfoImage() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const imageUrl = windowWidth >= 599 ? infoImage : infoImage_mobile;
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
   return (
-    <img className="info__image" src={infoImage} alt="peopleOnCrossroads" />
+    <img className="info__image" src={imageUrl} alt="peopleOnCrossroads" />
   );
 }
 
