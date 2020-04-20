a27da2b6e676cbfbf898abf95294b746146f58b6
Paulina Ziomkowska <paulina.ziomkowska@gmail.com>
2020-04-20 14:24:49 +0200

    Update SingleCard.js

--- src/Components/SingleCard/SingleCard.js
+++ src/Components/SingleCard/SingleCard.js
@@ -18,7 +18,7 @@ function SingleCard(props) {
           <p className="card_title">"{props.item.name}"</p>
           <p className="card_text">
             {props.item.city} || Dystans: {props.item.distance} km || Czas
-            trwania:{props.item.time} min
+            trwania: {props.item.time} min
           </p>
           <p className="card_text">Stopień trudności: {props.item.level}</p>
           <Grid>
